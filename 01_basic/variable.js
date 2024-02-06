076333534543d8bbf529c35b0c863a4ee40cff71
Anurag Gupta <anurag6967@gmail.com>
2024-02-07 00:42:24 +0530

    variables file added

---
+++ 01_basic/variable.js
@@ -0,0 +1,13 @@
+console.log("Welcome to the Javascript Anurag"); //to print the value
+
+// Variables
+const id='anurag'; //we can not re-declare and re-assign the value to the const variables
+let rollno=10; //we can re-assign the value but no re-declare the let variables
+var state='delhi'//try to do not use var keyword to eclare the variables because of block scope and functional scope
+pincode=110085; //we can declare the variable without using keyword but never used in pruduction level code
+
+// id=10; we can not reassign an value to const variables;
+rollno='ashutosh'
+state=20
+pincode="mumbai"
+console.table([id,rollno,state,pincode]);
