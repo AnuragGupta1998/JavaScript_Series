b99fc37a2eccf4d8af5d92a788c37459096b832b
Anurag Gupta <anurag6967@gmail.com>
2024-02-07 01:18:10 +0530

    data type of javascript

---
+++ 01_basic/data_type.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version of javascript
+
+/*
+number       1,2,3,19
+string       "Anurag"
+boolean      true/false
+null         standalone value
+undefined    value not assigned
+bigint       123456789123456789
+symbol       used for unique
+*/
+
+/*
+ //object
+ -an object
+ -an array
+ -a date
+*/
+
+// typeof is an operator used for type checking and returns the data type of the operand passed to it
+
+console.log(typeof null);    //null is typeof object
+console.log(typeof [1,2,3]); //array is typeof object
+console.log(typeof Symbol);  // symbol is typeof function
+
+console.log(typeof(1));
+
+
