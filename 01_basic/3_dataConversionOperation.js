f5fa9e48e5fd745b7371e0aaf6cc5608a914f4cc
Anurag Gupta <anurag6967@gmail.com>
2024-02-07 22:00:44 +0530

    added some opetions on data

--- 01_basic/3_dataConversionOperation.js
+++ 01_basic/3_dataConversionOperation.js
@@ -1,15 +1,15 @@
 let score=33
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 //Number Conversion
 let Name=""
 let isNumber=Number(Name);
-console.log(isNumber,typeof isNumber); //NaN(Not An Number) (typeof NaN is number)
+// console.log(isNumber,typeof isNumber); //NaN(Not An Number) (typeof NaN is number)
 let u=undefined
 let nu=Number(u);
-console.log('nu',nu);
+// console.log('nu',nu);
 
 /*
   "33"=> 33 number
@@ -25,7 +25,7 @@ console.log('nu',nu);
 //Boolean Demo
 let boolenaN="Anurag";
 let boolenaIs=Boolean(boolenaN);
-console.log(boolenaIs);
+// console.log(boolenaIs);
 
 /*
 1=>true boolean
@@ -38,7 +38,7 @@ console.log(boolenaIs);
 //String Conversion
 let numberS=undefined
 let Str=String(numberS)
-console.log(Str,typeof Str);
+// console.log(Str,typeof Str);
 
 /*
  33=>33 string
@@ -48,3 +48,43 @@ console.log(Str,typeof Str);
  undefined=>undefined string
 
 */
+
+//********************************************** OPERATIONS ON DATA  *************************************************
+let value=3;
+let negValue=-value;
+// console.log(negValue);
+
+// console.log(2+2); //4
+// console.log(2-2); //0
+// console.log(2*2); //4
+// console.log(2/3); //0.66666666
+// console.log(2**3); //2*2*2=8
+// console.log(2%9); //2 reminder
+
+//String operations
+let s1="Anurag"
+let s2=" Gupta"
+let s3=s1+s2
+// console.log(s3); //Anurag Gupta
+
+// console.log(1+"2"); //12
+// console.log("1"+2); //12
+// console.log(1+2+"5"); //35
+// console.log("1"+3+9); //139
+// console.log(1+"8"+9); //189
+
+// console.log(true+false);     //1+0=1
+// console.log(+true);          //1
+// console.log(true+false+"9"); //19
+// console.log(+""); //0
+// console.log(""); // empty space
+
+// postfix
+let a=3;
+let b=a++
+console.log(a,b);  //a=4 b=3
+
+prefix
+let c=5
+let d=++c  //
+console.log(c,d); //c=6 d=6
