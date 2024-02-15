ce83488924d44580eb0b2b74368d74fbd5144f23
Anurag Gupta <anurag6967@gmail.com>
2024-02-15 23:20:27 +0530

    de-structure of object

---
+++ 02_basic/5_object3.js
@@ -0,0 +1,25 @@
+console.log("De-Structure and JSON API");
+
+const course={
+    coursename:'Javascript',
+    price:2000,
+    courseInstructor:"Anurag"
+}
+//de-structure
+const {courseInstructor,price,coursename}=course
+
+console.log(coursename,price,courseInstructor);
+
+//customise the de-structuring
+const {courseInstructor:ci,price:p,coursename:cn}=course
+
+console.log(ci);
+
+//.....................JSON...............................
+//json demo
+// {
+//     "id":12345,
+//     "name":"Anurag",
+//     "email":"anurag@gmail.com"
+// }
+
