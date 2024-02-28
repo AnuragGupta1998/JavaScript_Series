a2a4945a02713b981cb840f83810c556233d6b92
Anurag Gupta <anurag6967@gmail.com>
2024-02-28 16:16:24 +0530

    if else and switch condition

---
+++ 04_conditional/if_else.js
@@ -0,0 +1,18 @@
+const val=100;
+//if()
+if(val>50){
+    console.log("greater than 50"); //greater than 50
+}
+
+//if()-else
+
+if(val<90){
+    console.log('less than 90');
+}
+else console.log('greater than 90');  //greater than 90
+
+//falsy values
+// false , 0 ,-0,bigInt 0n,"",undefined,NaN,null
+
+//truly values
+//true,"false"," ",[],{},"0"
---
+++ 04_conditional/switch.js
@@ -0,0 +1,33 @@
+const val=200;
+
+switch (val) {
+    case 100:
+        console.log('val is 100');
+        break;
+    case 200:
+        console.log('val is 200');
+        break;
+    default:
+        console.log('default executed');
+        break;
+}
+
+const month="march"
+
+switch (month) {
+    case 'january':
+        console.log('january');
+        break;
+    case 'february':
+        console.log('february');
+        break;
+    case 'march':
+        console.log('march');
+        break;
+    case 'april':
+        console.log('april');
+        break;
+    default:
+        console.log('default executed');
+        break;
+}
