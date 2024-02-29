d1e1ef84d88e1accd85bf9122c31a7192e117d34
Anurag Gupta <anurag6967@gmail.com>
2024-02-29 21:19:06 +0530

    reduce method of array

---
+++ 06_high_order_array_loop/reduce.js
@@ -0,0 +1,34 @@
+const arr=[1,2,3,4,5]
+
+// acc=accumalator currVal=current value
+const total=arr.reduce((acc,currVal)=>{
+    console.log(`acc:-${acc} currVal:-${currVal}`);
+    return acc+currVal},2) //what we pass after callback function is become accumalator
+console.log(total);
+
+//arrays of object
+const obj=[
+    {
+        name:"js",
+        price:299
+    },
+    {
+        name:"java",
+        price:2999
+    },
+    {
+        name:"python",
+        price:3999
+    },
+    {
+        name:"DSA",
+        price:499
+    },
+    {
+        name:"mobile",
+        price:5999
+    }
+]
+
+const totalPrice=obj.reduce((acc,item)=>acc+item.price,0);
+console.log(totalPrice);  //13795
